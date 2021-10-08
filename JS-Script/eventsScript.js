f4057cab62612a76f79a2fb1f7b9fcf18aad305e
MIVenturaCounseling <81735126+MIVenturaCounseling@users.noreply.github.com>
2021-10-07 21:30:54 -0500

    javascript for events page

---
+++ JS-Script/eventsScript.js
@@ -0,0 +1,7 @@
+var swch = document.getElementById("switchOne");
+//switch language to english by redirecting to other page with english text
+function switchLanguage() {
+    "use strict";
+    swch.checked = false;
+    window.location.href = "en/events.html";
+}
