49aafe05ee36f9aa0c33433a3d787b81833946ab
Cristian Ventura <99362764+cristian-ventura@users.noreply.github.com>
2023-02-02 20:34:14 -0600

    update therapy model spanish
    html
    images
    css
    js

--- JS-Script/therapyModelsScript.js
+++ JS-Script/therapyModelsScript.js
@@ -1,53 +1,62 @@
+var swch = document.getElementById("switchOne");
+var mobile_swch = document.getElementById("mobile_switchOne");
+
+//swch.checked = true;
+//mobile_swch.checked = true;
 //changes text color and background color of div to look as the selected tab of therapy models.
 //Also makes div with info appear
 function firstBlock() {
     "use strict";
     resetText();
-    document.getElementById("text1").style.color = "black";
-    document.getElementById("firstBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_one").style.backgroundColor = "#c4f4d4";
     document.getElementById("firstInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
 function secondBlock() {
     "use strict";
     resetText();
-    document.getElementById("text2").style.color = "black";
-    document.getElementById("secondBlock").style.backgroundColor = "white";
+    document.getElementById("last_box").style.backgroundColor = "#c4f4d4";
     document.getElementById("secondInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
+
 }
 function thirdBlock() {
     "use strict";
     resetText();
-    document.getElementById("text3").style.color = "black";
-    document.getElementById("thirdBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_six").style.backgroundColor = "#c4f4d4";
     document.getElementById("thirdInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
 function fourthBlock() {
     "use strict";
     resetText();
-    document.getElementById("text4").style.color = "black";
-    document.getElementById("fourthBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_five").style.backgroundColor = "#c4f4d4";
     document.getElementById("fourthInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
-function fifthBlock() {
+function fifthBlock() 
+{
     "use strict";
     resetText();
-    document.getElementById("text5").style.color = "black";
-    document.getElementById("fifthBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_four").style.backgroundColor = "#c4f4d4";
     document.getElementById("fifthInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
-function sixthBlock() {
+function sixthBlock() 
+{
     "use strict";
     resetText();
-    document.getElementById("text6").style.color = "black";
-    document.getElementById("sixthBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_three").style.backgroundColor = "#c4f4d4";
     document.getElementById("sixthInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
-function seventhBlock() {
+function seventhBlock() 
+{
     "use strict";
     resetText();
-    document.getElementById("text7").style.color = "black";
-    document.getElementById("seventhBlock").style.backgroundColor = "white";
+    document.getElementById("small_box_two").style.backgroundColor = "#c4f4d4";
     document.getElementById("seventhInfo").style.display = "block";
+    document.getElementById("top_of_info").scrollIntoView();
 }
 //resets all tabs to default of the therapy model tabs
 function resetText()
@@ -59,49 +68,137 @@ function resetText()
     document.getElementById("fifthInfo").style.display = "none";
     document.getElementById("sixthInfo").style.display = "none";
     document.getElementById("seventhInfo").style.display = "none";
-    
-    document.getElementById("text1").style.textDecoration = "none";
-    document.getElementById("text1").style.color = "white";
-    document.getElementById("firstBlock").style.backgroundColor = "cadetblue";
+
+    document.getElementById("small_box_one").style.backgroundColor = "rgb(156, 206, 208)";
    
+    document.getElementById("small_box_two").style.backgroundColor = "rgb(156, 206, 208)";
     
-    document.getElementById("text2").style.textDecoration = "none";
-    document.getElementById("text2").style.color = "white";
-    document.getElementById("secondBlock").style.backgroundColor = "cadetblue";
-    
-    
-    document.getElementById("text3").style.textDecoration = "none";
-    document.getElementById("text3").style.color = "white";
-    document.getElementById("thirdBlock").style.backgroundColor = "cadetblue";
+    document.getElementById("small_box_three").style.backgroundColor = "rgb(156, 206, 208)";
    
+    document.getElementById("small_box_four").style.backgroundColor = "rgb(156, 206, 208)";
     
-    document.getElementById("text4").style.textDecoration = "none";
-    document.getElementById("text4").style.color = "white";
-    document.getElementById("fourthBlock").style.backgroundColor = "cadetblue";
+    document.getElementById("small_box_five").style.backgroundColor = "rgb(156, 206, 208)";
     
-    
-    document.getElementById("text5").style.textDecoration = "none";
-    document.getElementById("text5").style.color = "white";
-    document.getElementById("fifthBlock").style.backgroundColor = "cadetblue";
-    
-    
-    document.getElementById("text6").style.textDecoration = "none";
-    document.getElementById("text6").style.color = "white";
-    document.getElementById("sixthBlock").style.backgroundColor = "cadetblue";
-   
-    
-    document.getElementById("text7").style.textDecoration = "none";
-    document.getElementById("text7").style.color = "white";
-    document.getElementById("seventhBlock").style.backgroundColor = "cadetblue";
+    document.getElementById("small_box_six").style.backgroundColor = "rgb(156, 206, 208)";
+ 
+    document.getElementById("last_box").style.backgroundColor = "rgb(156, 206, 208)";
     
 }
 
-var swch = document.getElementById("switchOne");
+
 
 //switches to to english texts by redirecting to english page
 function switchLanguage() {
     "use strict";
     swch.checked = false;
+    mobile_swch = false;
     window.location.href = "en/therapyModels.html";
 }
 
+
+
+var browser_width = window.innerWidth;
+var browser_height = window.innerHeight;
+
+var screen_width = screen.width;
+var screen_height = screen.height;
+
+
+function checkScreen()
+{
+    'use strict';
+    var limit_of_screen = 900;
+    
+                        if(browser_width <= limit_of_screen)
+                        {
+                           
+                            //Adjust container of each row of blocks
+                            document.getElementById("small_boxes_container_one").style.width = "90%";
+                            document.getElementById("small_boxes_container_two").style.width = "90%";
+                            document.getElementById("last_box_container").style.width = "90%";
+                            //set box width back to normal
+                            document.getElementById("small_box_one").style.width = "31%"; 
+                            document.getElementById("small_box_two").style.width = "31%";
+                            document.getElementById("small_box_three").style.width = "31%"; 
+                            document.getElementById("small_box_four").style.width = "31%"; 
+                            document.getElementById("small_box_five").style.width = "31%"; 
+                            document.getElementById("small_box_six").style.width = "31%";
+                            document.getElementById("last_box").style.width = "31%"; 
+                            //set dividers space back to normal
+                            document.getElementById("row_one_small_divider_one").style.width = "3.5%";
+                            document.getElementById("row_one_small_divider_two").style.width = "3.5%";
+                            document.getElementById("row_two_small_divider_one").style.width = "3.5%";
+                            document.getElementById("row_two_small_divider_two").style.width = "3.5%";
+                          
+                           
+
+                            
+                            
+                            if(browser_width <= 700)
+                            {
+                                //set box width to 100% for minimized window
+                                document.getElementById("small_box_one").style.width = "100%"; 
+                                document.getElementById("small_box_two").style.width = "100%";
+                                document.getElementById("small_box_three").style.width = "100%"; 
+                                document.getElementById("small_box_four").style.width = "100%"; 
+                                document.getElementById("small_box_five").style.width = "100%"; 
+                                document.getElementById("small_box_six").style.width = "100%";
+                                document.getElementById("last_box").style.width = "100%";
+                                //adjust width of dividers so doesnt leave space between each box when minimized
+                                document.getElementById("row_one_small_divider_one").style.width = "0%";
+                                document.getElementById("row_one_small_divider_two").style.width = "0%";
+                                document.getElementById("row_two_small_divider_one").style.width = "0%";
+                                document.getElementById("row_two_small_divider_two").style.width = "0%";
+                                
+
+                                
+
+                            }
+                            //if(browser_width <= 500)
+                            //{
+                                
+                               
+                            //}
+                          
+                          
+                            
+                            
+                        }
+                        if((screen_width > limit_of_screen) && (browser_width > limit_of_screen))
+                        {
+                            //Adjust container of each row of blocks
+                            document.getElementById("small_boxes_container_one").style.width = "75%";
+                            document.getElementById("small_boxes_container_two").style.width = "75%";
+                            document.getElementById("last_box_container").style.width = "75%";
+                            //set box width back to normal
+                            document.getElementById("small_box_one").style.width = "31%"; 
+                            document.getElementById("small_box_two").style.width = "31%";
+                            document.getElementById("small_box_three").style.width = "31%"; 
+                            document.getElementById("small_box_four").style.width = "31%"; 
+                            document.getElementById("small_box_five").style.width = "31%"; 
+                            document.getElementById("small_box_six").style.width = "31%";
+                            document.getElementById("last_box").style.width = "31%";   
+                            //set dividers space back to normal
+                            document.getElementById("row_one_small_divider_one").style.width = "3.5%";
+                            document.getElementById("row_one_small_divider_two").style.width = "3.5%";
+                            document.getElementById("row_two_small_divider_one").style.width = "3.5%";
+                            document.getElementById("row_two_small_divider_two").style.width = "3.5%";
+
+                            
+                        }
+
+}
+
+checkScreen();
+
+//checks to see if browser is resized
+window.addEventListener('resize', 
+    function(event){
+                     
+                        browser_width = window.innerWidth;
+                        browser_height = window.innerHeight;
+                        screen_width = screen.width;
+                        screen_height = screen.height;
+                        checkScreen();
+                        
+                    });
